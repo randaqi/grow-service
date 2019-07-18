985c7fd86c6793208cac98b5b8968e5b13ddacd1
Zephyr Mei <zephyr.mei@oocl.com>
2019-07-18 15:15:23 +0800

    Add a new test in WeeklyPlanRepository.

--- src/test/java/com/oocl/grow/repository/WeeklyPlanRepositoryIntegrationTest.java
+++ src/test/java/com/oocl/grow/repository/WeeklyPlanRepositoryIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class WeeklyPlanRepositoryIntegrationTest {
     @Autowired
     private TestEntityManager testEntityManager;
@@ -22,14 +24,22 @@ public class WeeklyPlanRepositoryIntegrationTest {
     private WeeklyPlanRepository weeklyPlanRepository;
 
     @Test
-    private void add_new_Weekly_plan_successfully(){
+
+    public void add_new_Weekly_plan_successfully(){
         for (int i = 0; i < 4; i++) {
-            testEntityManager.persist(WeeklyPlan.builder().weeklyPlanId(i).build());
+            testEntityManager.persist(WeeklyPlan.builder().build());
             testEntityManager.flush();
         }
-
         List<WeeklyPlan> weeklyPlans = weeklyPlanRepository.findAll();
         assertThat(weeklyPlans.size()).isEqualTo(4);
-        assertThat(weeklyPlans.get(2).getWeeklyPlanId()).isEqualTo(2);
+        assertThat(weeklyPlans.get(3).getWeeklyPlanId()).isEqualTo(4);
+    }
+
+    @Test
+    public void return_NULL_when_not_find_Weekly_Plan_by_id(){
+        WeeklyPlan weeklyPlan = WeeklyPlan.builder().build();
+        testEntityManager.persist(weeklyPlan);
+        testEntityManager.flush();
+        assertThat(weeklyPlanRepository.findByWeeklyPlanId(2)).isNull();
     }
 }
